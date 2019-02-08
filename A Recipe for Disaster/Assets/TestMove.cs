cf4360db4b0038c02a8a02b2b43be467355986b2
Robin <friedmr5@tcnj.edu>
2019-02-08 13:26:45 -0500

    Successful Box Collider test
    A 2D sprite in a 3D world can have a box collider (3D) and have it obey 3D physics.

--- A Recipe for Disaster/Assets/TestMove.cs
+++ A Recipe for Disaster/Assets/TestMove.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class TestMove : MonoBehaviour {
 
+    Rigidbody _rb;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        _rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float move = Input.GetAxis("Vertical");
+        Vector3 move3 = new Vector3(0, 0, move);
+        _rb.AddForce(move3, ForceMode.Impulse);
+    }
 }
