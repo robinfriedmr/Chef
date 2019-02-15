6dd66d7f338208811c9852754b69e9a9a731c1ef
Robin Friedman <friedmr5@tcnj.edu>
2019-02-15 15:29:53 -0500

    Scene Changing and Reposition
    Added organization to the Assets folder. Added a new scene for Battle. Preserves gameobjects "Player" and "Enemy" for new "BattleScene" and repositions them. (Player rotates anyway?)

---
+++ A Recipe for Disaster/Assets/Scripts/BattleReady.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleReady : MonoBehaviour {
+
+    GameObject enemyEncounter;
+    public Scene battle;
+
+    Vector3 battlingPlayer;
+    Vector3 battlingEnemy;
+
+
+	void Start () {
+        battlingPlayer = new Vector3(-6, 2, 1);
+        battlingEnemy = new Vector3(6, 2, 1);
+    }
+	
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+        Debug.Log("Collision detected.");
+
+        // Keep these when loading battle scene!
+        enemyEncounter = collision.gameObject;
+        DontDestroyOnLoad(enemyEncounter);
+        DontDestroyOnLoad(this.gameObject);
+
+        // But move the player and enemy into position.
+        Reposition(collision);
+
+        // Switch scenes on collision
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if (sceneName != "BattleScene") {
+            SceneManager.LoadScene("BattleScene", LoadSceneMode.Single);
+        }
+        Debug.Log("Active scene: " + SceneManager.GetActiveScene().name);
+            }
+
+    void Reposition (Collision enemy) {
+        // Stabilize Player
+        GetComponent<Transform>().position = battlingPlayer;
+
+        Rigidbody enemyBody = GetComponent<Rigidbody>();
+        enemyBody.velocity = new Vector3(0, 0, 0);
+        enemyBody.rotation = new Quaternion(0, 0, 0, 0); // NOT WORKING*****
+
+        // Stabilize Enemy
+        enemy.gameObject.GetComponent<Transform>().position = battlingEnemy;
+
+        // (no rigidbody!)
+        //enemy.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+    }
+}
