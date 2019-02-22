f749fc4e8055e13a0c47c3c2dbfe0535d4324c90
Robin Friedman <friedmr5@tcnj.edu>
2019-02-22 13:33:52 -0500

    Camera Follow Script
    Added Chad's script for camera movement. Looks solid.

---
+++ A Recipe for Disaster/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
+}
