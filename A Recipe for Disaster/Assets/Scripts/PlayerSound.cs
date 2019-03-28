199bd0a7aa301cbcc18d1ff2cf10f235e3d7c647
fpg246 <friedmr5@tcnj.edu>
2019-03-28 13:23:50 -0400

    Footstep Sounds Synced
    Animation is now synced with the sound of a footstep at each event, placed according to the frames of the animation.  Replaced chef_move_east animation because I don't know how to edit existing animations and it had a frame order of 0, 0, 2, 3, 4, 5 rather than 0 - 5. Total worktime ~1 hour.

---
+++ A Recipe for Disaster/Assets/Scripts/PlayerSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour {
+
+    public AudioClip footStep;
+    public AudioSource audioS;
+
+	void Step ()
+    {
+        audioS.PlayOneShot(footStep);
+    }
+}
