ec769534b074c684c8d5a6c390b3a1a7309d9626
Kenny Lepping <kennylepping@gmail.com>
2019-04-04 23:08:07 -0400

    Testing Audio Scripts

---
+++ A Recipe for Disaster/Assets/MixLevels.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixLevels : MonoBehaviour {
+
+    public AudioMixer masterMixer;
+
+   /* public void setGlitchLvl(float glitchLvl)
+    {
+        masterMixer.SetFloat("Glitch Track", glitchLvl);
+    }
+
+    public void setAprtmntMusicLvl(float musicLvl)
+    {
+        masterMixer.SetFloat("Amibent Music", musicLvl);
+    } */
+
+    public void SetVolume(float volume)
+    {
+        masterMixer.SetFloat("volume", volume);
+    }
+}
+
