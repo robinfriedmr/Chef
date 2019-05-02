3e4eeb5993f26ef67ff5c03a4fbbb0224c96bc4e
fpg246 <friedmr5@tcnj.edu>
2019-05-02 13:38:10 -0400

    Aqua Footsteps
    Imported Kenny's update with Aqua's footsteps (from branch: Audio3). Worktime ~0.25 hours.

--- A Recipe for Disaster/Assets/Scripts/AquaSound.cs
+++ A Recipe for Disaster/Assets/Scripts/AquaSound.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSound : MonoBehaviour {
+public class AquaSound : MonoBehaviour {
 
-    public AudioClip footStep;
+    public AudioClip aquaStep;
     public AudioSource audioS;
 
-	void Step ()
+    void Step()
     {
         audioS.volume = Random.Range(0.8f, 1f); // experiment if desired
         audioS.pitch = Random.Range(1.1f, 1.4f); // experiment if desired
-        audioS.PlayOneShot(footStep);
+        audioS.PlayOneShot(aquaStep);
     }
 }
