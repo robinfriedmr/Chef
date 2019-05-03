12a6e3f2b2b5100f6f16384999a691613e68e9b2
Robin Friedman <friedmr5@tcnj.edu>
2019-05-03 13:51:06 -0400

    Menu Sound, Footsteps Mix
    Worktime ~0.5 hours.

---
+++ A Recipe for Disaster/Assets/Inventory/ClickSound.cs
@@ -0,0 +1,17 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ClickSound : MonoBehaviour, IPointerDownHandler
+{
+
+    public AudioClip clickSound;
+    public AudioSource audioSource;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(clickSound);
+    }
+}
